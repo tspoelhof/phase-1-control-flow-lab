307c039b9899eec72726e6735ad7965d36314b17
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-08-08 22:21:14 -0400

    a

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet >= 400 && feet <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+    else if (feet > 2000 && feet <=2500) {
+      return 'I will gladly take your thirty bucks.';
+    }
+    else {
+      return 'No can do.';
+    }
+  }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.';
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+        case 'not as generous':
+          return 'Thank you.';
+            default:
+              return 'Bye.';
+  }
 }
